659042bdaeba631e4c7bcab1c7c5b29ffc55752e
KatoJunichi <X42Revolution@gmail.com>
2025-12-16 16:56:44 +0900

    2_14 完了

--- curriculum_2_14/Program.cs
+++ curriculum_2_14/Program.cs
@@ -7,12 +7,26 @@ namespace curriculum_2_14
         static void Main(string[] args)
         {
             // ①配列の宣言
-            var myArray = new[] {"aaa", "bbb", "ccc", "ddd", "abc"};
-            
+            var myArray = new[] { "aaa", "bbb", "ccc", "ddd", "abc" };
+
             Console.WriteLine(myArray.Contains("aaa")
                 ? "含んでいます"
                 : "含んでいません"
                 );
+
+            // ②listの宣言
+            var bird = new List<string>();
+            bird.Add("crow");
+            bird.Add("sparrow");
+            bird.Add("swallow");
+            bird.Add("pigeon");
+            bird.Remove("sparrow");
+            bird.RemoveAt(1);
+            foreach (var b in bird)
+            {
+                Console.WriteLine(b);
+
+            }
         }
     }
 }
