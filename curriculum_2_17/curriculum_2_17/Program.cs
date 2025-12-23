b0f6ea4e90451799cbe6884341eb268d07117b45
KatoJunichi <X42Revolution@gmail.com>
2025-12-23 14:10:37 +0900

    curriculum_2_17 完了

--- curriculum_2_17/curriculum_2_17/Program.cs
+++ curriculum_2_17/curriculum_2_17/Program.cs
@@ -7,6 +7,15 @@ namespace curriculum_2_17
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            // ②コンソールで2つの文字列を入力し、その文字列を結合した文字列を出力するプログラム
+            Console.Write("コンソールに文字列１を入力：");
+            string str1 = Console.ReadLine();
+            Console.Write("コンソールに文字列２を入力：");
+            string str2 = Console.ReadLine();
+            // 文字列を結合する
+            string result = string.Concat(str1, str2);
+            Console.WriteLine("出力：" + result);
         }
     }
 }
