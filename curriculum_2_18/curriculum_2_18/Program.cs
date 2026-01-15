619fe767a5c68129c2614a465491882c9a9536ad
KatoJunichi <X42Revolution@gmail.com>
2026-01-15 19:35:32 +0900

    curriculum_2_18 完了

--- curriculum_2_18/curriculum_2_18/Program.cs
+++ curriculum_2_18/curriculum_2_18/Program.cs
@@ -2,11 +2,26 @@
 
 namespace curriculum_2_18
 {
+    // Employeeクラスの定義
+    class Employee
+    {
+        // 自動プロパティの宣言
+        public string EmployeeId { get; set; }
+        public string Name { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-           
+            // Employeeクラスのインスタンスを作成し、プロパティに値を設定
+            Employee emp = new Employee
+            {
+                EmployeeId = "E002",
+                Name = "田中花子"
+            };
+            // プロパティの値を表示
+            Console.WriteLine($"社員ID: {emp.EmployeeId}, 名前: {emp.Name}");
         }
     }
 }
