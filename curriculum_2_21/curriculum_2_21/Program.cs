69b7ed6e9c0a51a854cb6cfdd60c3ba4339a5d85
KatoJunichi <X42Revolution@gmail.com>
2026-02-16 14:28:58 +0900

    first commit

---
+++ curriculum_2_21/curriculum_2_21/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace curriculum_2_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
